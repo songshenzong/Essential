8dd557dbe1a67052ad9f4647cbfd0c45436effc2
Songshenzong <i@songshenzong.com>
2018-03-15 23:24:14 +0800

    support test

--- src/Traits/BashEcho.php
+++ src/Traits/BashEcho.php
@@ -7,65 +7,65 @@ trait BashEcho
     /**
      * @param $string
      */
-    public function echoRed($string)
+    public function echoRed($string): void
     {
         $cmd = "echo -ne \"\033[31m" . $string . " \033[0m\n\"";
         $a   = exec($cmd);
-        print "$a" . PHP_EOL;
+        print (string) $a . PHP_EOL;
     }
 
 
     /**
      * @param $string
      */
-    public function echoGreen($string)
+    public function echoGreen($string): void
     {
         $cmd = "printf \"\033[32m" . $string . "\033[0m\n\"";
         $a   = exec($cmd);
-        print "$a" . PHP_EOL;
+        print (string) $a . PHP_EOL;
     }
 
 
     /**
      * @param $string
      */
-    public function echoBrown($string)
+    public function echoBrown($string): void
     {
         $cmd = "printf \"\033[33m" . $string . "\033[0m\n\"";
         $a   = exec($cmd);
-        print "$a" . PHP_EOL;
+        print (string) $a . PHP_EOL;
     }
 
 
     /**
      * @param $string
      */
-    public function echoBlue($string)
+    public function echoBlue($string): void
     {
         $cmd = "printf \"\033[34m" . $string . "\033[0m\n\"";
         $a   = exec($cmd);
-        print "$a" . PHP_EOL;
+        print (string) $a . PHP_EOL;
     }
 
 
     /**
      * @param $string
      */
-    public function echoPurple($string)
+    public function echoPurple($string): void
     {
         $cmd = "printf \"\033[35m" . $string . "\033[0m\n\"";
         $a   = exec($cmd);
-        print "$a" . PHP_EOL;
+        print (string) $a . PHP_EOL;
     }
 
 
     /**
      * @param $string
      */
-    public function echoCyan($string)
+    public function echoCyan($string): void
     {
         $cmd = "printf \"\033[35m" . $string . "\033[0m\n\"";
         $a   = exec($cmd);
-        print "$a" . PHP_EOL;
+        print (string) $a . PHP_EOL;
     }
 }
--- tests/Str/StrTest.php
+++ tests/Str/StrTest.php
@@ -2,8 +2,10 @@
 
 namespace Songshenzong\Support\Str;
 
+use function echoBlue;
 use PHPUnit\Framework\TestCase;
 use Songshenzong\Support\Strings;
+use stdClass;
 
 
 class StrTest extends TestCase
@@ -32,6 +34,17 @@ ETO;
      */
     protected $jsonString = '{ "tools": [ { "name":"css format" , "site":"https://songshenzong.com" }, { "name":"pwd check" , "site":"https://songshenzong.com" } ] }';
 
+
+    /**
+     * @var array
+     */
+    protected $simpleJsonArray = ['json' => true];
+
+    /**
+     * @var string
+     */
+    protected $simpleJsonString = '{"json":true}';
+
     /**
      * @throws \PHPUnit\Framework\ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -41,7 +54,7 @@ ETO;
         $this->assertEquals(false, Strings::isJson());
         $this->assertEquals(false, Strings::isJson(''));
         $this->assertEquals(false, Strings::isJson($this->xmlString));
-        $this->assertEquals(true, Strings::isJson('{"json":true}'));
+        $this->assertEquals(true, Strings::isJson($this->simpleJsonString));
         $this->assertEquals(true, Strings::isJson($this->jsonString));
     }
 
@@ -56,7 +69,40 @@ ETO;
         $this->assertEquals(false, Strings::isXml());
         $this->assertEquals(false, Strings::isXml($this->jsonString));
         $this->assertEquals(true, Strings::isXml($this->xmlString));
-        $this->assertEquals(false, Strings::isXml('{"json":true}'));
+        $this->assertEquals(false, Strings::isXml($this->simpleJsonString));
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     */
+    public function testToArray(): void
+    {
+        $this->assertEquals($this->simpleJsonArray, Strings::toArray($this->simpleJsonString));
+        $this->assertEquals([], Strings::toArray());
+        $this->assertEquals([], Strings::toArray(''));
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     */
+    public function testToObject(): void
+    {
+        $this->assertEquals(true, Strings::toObject($this->simpleJsonString)->json);
+        $this->assertObjectHasAttribute('book', Strings::toObject($this->xmlString));
+        $this->assertEquals(new stdClass(), Strings::toObject());
+        $this->assertEquals(new stdClass(), Strings::toObject(''));
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     */
+    public function testXmlToArray(): void
+    {
+        $this->assertArrayHasKey('book', Strings::xmlToArray($this->xmlString));
+        $this->assertObjectHasAttribute('book', Strings::xmlToObject($this->xmlString));
     }
 
 
