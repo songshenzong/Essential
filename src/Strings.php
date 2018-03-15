31fd7bc23bd7be93e121988d32d1acd0fe1f01b7
Songshenzong <i@songshenzong.com>
2018-03-15 15:50:16 +0800

    support xml

---
+++ src/Strings.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Songshenzong\Support;
+
+use Songshenzong\Support\Traits\Str;
+
+class Strings
+{
+    use Str;
+}
--- src/Traits/Str.php
+++ src/Traits/Str.php
@@ -2,14 +2,35 @@
 
 namespace Songshenzong\Support\Traits;
 
+use SimpleXMLElement;
+
 trait Str
 {
+
+
     /**
-     * @param $str
+     * @param string $string
+     *
+     * @return bool
+     */
+    public static function isSerialized(string $string): bool
+    {
+        // Set Handle
+        set_error_handler(function () {
+        }, E_ALL);
+        $result = unserialize($string);
+        // Restores the previous error handler function
+        restore_error_handler();
+        return !($result === false);
+    }
+
+
+    /**
+     * @param string $string
      *
      * @return string
      */
-    public function stringFilter($str): string
+    public static function stringFilter(string $string): string
     {
         $filter = [
             "\n",
@@ -68,17 +89,17 @@ trait Str
             'nbsp',
         ];
 
-        $str = str_replace($filter, '', $str);
+        $str = str_replace($filter, '', $string);
         return trim($str);
     }
 
 
     /**
-     * @param $str
+     * @param string $string
      *
      * @return string
      */
-    public function stringTrim($str): string
+    public static function stringTrim(string $string): string
     {
         $filter = [
             "\0",
@@ -89,7 +110,7 @@ trait Str
             ' ',
         ];
 
-        $str = str_replace($filter, '', $str);
+        $str = str_replace($filter, '', $string);
         return trim($str);
     }
 
@@ -101,7 +122,7 @@ trait Str
      *
      * @return bool
      */
-    public function isSetAndNotEmpty($value): bool
+    public static function isSetAndNotEmpty($value): bool
     {
         return isset($value) && !empty($value);
     }
@@ -113,8 +134,67 @@ trait Str
      *
      * @return bool
      */
-    public function isSetAndNotEmptyAndNotNull($value): bool
+    public static function isSetAndNotEmptyAndNotNull($value): bool
     {
         return isset($value) && !empty($value) && $value !== 'null';
+    }
+
+
+    /**
+     * @param string $string
+     *
+     * @return bool
+     */
+    public static function isXml(string $string): bool
+    {
+        $xml_parser = xml_parser_create();
+        if (!xml_parse($xml_parser, $string, true)) {
+            xml_parser_free($xml_parser);
+            return false;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * @param string $string
+     *
+     * @return array
+     */
+    public static function xmlToArray(string $string): array
+    {
+        return json_decode(json_encode(simplexml_load_string($string)), true);
+    }
+
+
+    /**
+     * @param string $string
+     *
+     * @return SimpleXMLElement
+     */
+    public static function xmlToObject(string $string): SimpleXMLElement
+    {
+        return simplexml_load_string($string);
+    }
+
+
+    /**
+     * @param string $string
+     *
+     * @return bool
+     */
+    public static function isJson(string $string): bool
+    {
+        if ($string === '') {
+            return false;
+        }
+
+        \json_decode($string);
+        if (\json_last_error()) {
+            return false;
+        }
+
+        return true;
     }
 }
